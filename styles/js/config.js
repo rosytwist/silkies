47a20221f5c7e942878c50e858eda4d59580292d
rosytwist <rosy.twist.art@gmail.com>
2026-03-21 16:52:55 -0500

    Update config.js

--- styles/js/config.js
+++ styles/js/config.js
@@ -45,7 +45,7 @@ charadex.sheet = {
     designTypes: ['All', 'Official Design', 'Guest Design', 'MYO Slot', 'MYO Design'],
     statuses: ['All', 'Resell', 'Trade', 'Gift', 'Voided', 'For Sale', 'Purchased'],
     rarity: ['All', 'Common', 'Uncommon', 'Rare', 'Very Rare', 'Legendary'],
-    species: ['All', 'Dog', 'Cat', 'Bunny'],
+    species: ['All', 'Silkies', 'Nautiloomis', 'Avians', 'Astronae', 'Moth', 'Draskulin'],
     itemTypes: ['All', 'Currency', 'MYO Slot', 'Pet', 'Trait', 'Misc'],
     traitTypes: ['All', 'Ears', 'Eyes', 'Body', 'Limbs', 'Tails', 'Misc', 'Mutations']
 
