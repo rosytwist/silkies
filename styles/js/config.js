18a4a83c66b4e0375d0d72cc6b6cbaac0c0c6d82
rosytwist <rosy.twist.art@gmail.com>
2026-03-21 15:45:17 -0500

    Update config.js

--- styles/js/config.js
+++ styles/js/config.js
@@ -26,7 +26,7 @@ charadex.site = {
 /* ==================================================================== */
 charadex.sheet = {
 
-  id: "1GwgfLizD3HQCieGia6di-TfU4E3EipT9Jb0BDZQwNak",
+  id: "1LDg-pDsQ1ZsTs9VnjASLcp7UpBGvLAJGOgUOMHpJux0",
 
   pages: {
     masterlist:    "masterlist",
